77aea0f03a548de14c23aa31db8d4da7ab2f322f
Pablo Castilho <pabloccard@gmail.com>
2026-09-22 21:44:48 -0300

    trackemaneto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,11 +38,7 @@ export default function RootLayout({
         {/* Google Tag Manager / Script de Trackeamento Customizado */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s);j.async=true;j.src=
-            "https://api.marianasalazar.site/1hlacdxvzy.js?"+i;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','bhjsn=EB5LPzAuTDgpPSwrUSAqSg9HRFZVRQUCRBcXCg0CDw0KGxoZHgITQgoTAh0%3D');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://api.marianasalazar.site/1hlacdxvzy.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','bhjsn=EB5LPzAuTDgpPSwrUSAqSg9HRFZVRQUCRBcXCg0CDw0KGxoZHgITQgoTAh0%3D');
           `}
         </Script>
       </head>
